a8bf2a8ed323bc02f4730015fb04d66ce335e976
unknown <olyhaa@.(none)>
2010-10-21 19:17:42 -0400

    first test

---
+++ Main.java
@@ -0,0 +1,18 @@
+package gui;
+
+/**
+ * Main class for the RPI Housing application.
+ * 
+ * Creates window and begins running
+ *
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		App app = new App();
+		
+		app.run();
+		
+	}
+
+}
